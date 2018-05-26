984d6ce7c470d41e593050dad0374ebb9085025e
Suma Sree Thota <36622064+sumasree98@users.noreply.github.com>
2018-05-26 10:53:03 +0530

    Add files via upload

---
+++ hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+ 
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+ 
+ 
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+ 
+  getHero(id: number): Observable<Hero> {
+    return of(HEROES.find(hero => hero.id === id));
+  }
+  
+}
---
+++ mock-heroes.ts
@@ -0,0 +1,54 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {
+        id:1,
+        name: "Professor X",
+        superPower: "Mind Control"
+    },
+    {
+        id: 2,
+        name: "Wolverine",
+        superPower: "Super human strength and claws"
+    },
+    {
+        id:3,
+        name: "Mystique",
+        superPower: "Metamorphism"
+    },
+    {
+        id:4,
+        name: "Magneto",
+        superPower: "Magnokinesis"
+    },
+    {
+        id:5,
+        name: "Cyclops",
+        superPower: "Powerful beam generation from eyes"
+    },
+    {
+        id:6,
+        name: "Storm",
+        superPower: "Weather Manipulation"
+    },
+    {
+        id:7,
+        name: "Pyro",
+        superPower: "Pyrokinesis"
+    },
+    {
+        id:8,
+        name: "Beast",
+        superPower: "Super human strength"
+    },
+    {
+        id:9,
+        name: "Phoenix",
+        superPower: "Telekinesis"
+    },
+    {
+        id:10,
+        name: "Deadpool",
+        superPower: "Super healing"
+    }
+];
